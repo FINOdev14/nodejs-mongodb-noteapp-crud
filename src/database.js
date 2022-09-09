3a341fb2e82377cfc31ce714f8e7c2c7531d7816
Dev14 <dev14@codecraftdev.com>
2022-09-09 13:50:04 -0500

    cambi19

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE, MONGODB_URI} = process.env;
+//const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE, MONGODB_URI} = process.env;
 //const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
-
+const MONGODB_URI = "mongodb+srv://kevin:12345@cluster0.6xvoq7h.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,

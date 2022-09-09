9c250dd7aa9b854d13f56f412fadea6c40881767
Dev14 <dev14@codecraftdev.com>
2022-09-09 14:07:06 -0500

    cambiodeurl

--- src/database.js
+++ src/database.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 //const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 const MONGODB_URI = "mongodb+srv://kevin:12345@cluster0.6xvoq7h.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(MONGODB_URI, {
-    useUnifiedTopology: true,
+ mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true
+    
     //useCreateIndex: true
 })
     .then(db => console.log('DataBase is connected'))
     .catch(err => console.log(err));
+
